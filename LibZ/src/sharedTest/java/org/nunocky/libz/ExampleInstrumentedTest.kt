2f84f1e8a2d7586953c517fa62285965e356997e
Masato NUNOKAWA <masato.nunokawa@gmail.com>
2022-01-30 19:19:15 +0900

    テスト修正

--- LibZ/src/sharedTest/java/org/nunocky/libz/ExampleInstrumentedTest.kt
+++ LibZ/src/sharedTest/java/org/nunocky/libz/ExampleInstrumentedTest.kt
@@ -1,6 +1,8 @@
 package org.nunocky.libz
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import junit.framework.TestCase.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -11,12 +13,12 @@ import org.junit.runner.RunWith
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
-//    @Test
-//    fun useAppContext() {
-//        // Context of the app under test.
-//        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-//        assertEquals("org.nunocky.libz.test", appContext.packageName)
-//    }
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("org.nunocky.libz.test", appContext.packageName)
+    }
 
     @Test
     fun callHello() {
